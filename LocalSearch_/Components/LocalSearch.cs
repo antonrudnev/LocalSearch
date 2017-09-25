3b225c0b8fd9e961e06b7bfd4a965fad28d4ae1e
Anton Rudnev <anton.rudnev@gmail.com>
2017-09-25 00:57:06 -0400

    Clean solution

--- LocalSearch_/Components/LocalSearch.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace LocalSearch.Components
-{
-    public abstract class LocalSearch<T> where T : ISolution
-    {
-        public abstract IEnumerable<T> Minimize(T solution);
-    }
-}
--- LocalSearch_/Components/MultistartOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LocalSearch.Components
-{
-    public class MultistartOptions
-    {
-        public int InstancesNumber { get; set; } = 1;
-
-        public int OutputDelayInMilliseconds { get; set; } = 100;
-    }
-}
